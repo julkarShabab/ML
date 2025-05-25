cbc1255f1d48cda479d69f77272c1a85cfd11e67
julkarShaba <julkarniene@gmail.com>
2025-05-26 00:10:24 +0600

    till fixing missing data

--- practice.py
+++ practice.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:,:-1].values
-print(x)
+#print(x)
 y = dataset.iloc[:,-1].values
-print(y)
+#print(y)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean') # missing values are locatedd and their values get replaced by the preferred strategy
+imputer.fit(x[:,1:3]) #from column 1 to column 2 since python excludes the upper value
+x[:,1:3] = imputer.transform(x[:,1:3])
+print(x)
