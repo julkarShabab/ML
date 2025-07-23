23bdb5b13aca2db803f2347feb9f1cc7fd2f9fed
julkarShaba <julkarniene@gmail.com>
2025-07-23 16:14:39 +0600

    till feature scaling

--- practice.py
+++ practice.py
@@ -22,9 +22,33 @@ ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[0])],remainder=
 #what kind of encoding,what kind pf encoding,coolumn index
 #remainder = passthrough means other columns will remain intact
 x = np.array(ct.fit_transform(x)) #converts the matrix into numpy array
-print(x)
+# print(x)
 
 from sklearn.preprocessing import LabelEncoder #label encoder one at a time -->> binary outcome
 le = LabelEncoder() #convert categorical labels into integer values
 y = le.fit_transform(y)
-print(y)
+# print(y)
+
+ 
+#splitting data
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(x,y,test_size = 0.2, random_state=1)
+print("x train",X_train)
+print("x test",X_test)
+print(Y_train)
+print(Y_test)
+print(">>>>>>")
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:,3:] = sc.fit_transform(X_train[:,3:]) #here we're taking the age and the salary column only
+X_test[:,3:] = sc.transform(X_test[:,3:])
+print(X_train)
+print(">>>")
+print(X_test)
+
+#| Data      | What you use       | Why                     |
+#| --------- | ------------------ | ----------------------- |
+#| `X_train` | `fit_transform()`  | Learn + scale the data  |
+#| `X_test`  | `transform()` only | Just scale, no learning |
